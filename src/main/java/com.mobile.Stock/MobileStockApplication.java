15e13a21c897a2f432fbc62dbe00a9f20b7c61ca
MilonNandi99 <161121165+MilonNandi99@users.noreply.github.com>
2024-05-05 17:12:15 +0530

    Create MobileStockApplication.java

---
+++ src/main/java/com.mobile.Stock/MobileStockApplication.java
@@ -0,0 +1,18 @@
+package com.mobileStock;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileStockApplication.class, args);
+	}
+
+}
+
+
+
+
