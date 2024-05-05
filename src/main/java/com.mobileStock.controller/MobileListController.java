9e193d2a4ab2141d5df945f6061342cee6a57675
MilonNandi99 <161121165+MilonNandi99@users.noreply.github.com>
2024-05-05 17:26:05 +0530

    Create MobileListController.java

---
+++ src/main/java/com.mobileStock.controller/MobileListController.java
@@ -0,0 +1,21 @@
+package com.mobileStock.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mobileStock.service.MobileListService;
+
+@Controller
+public class MobileListController {
+
+	@Autowired
+	private MobileListService service;
+	
+	@RequestMapping("/deleteMyList/{id}")
+	public String deleteMyList(@PathVariable("id") int id) {
+		service.deleteById(id);
+		return "redirect:/my_mobiles";
+	}
+}
